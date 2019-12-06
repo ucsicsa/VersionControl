a7c73c93a3c80b91e8b312d28c115e241d9af442
Martin Horváth <martin.horvath3@stud.uni-corvinus.hu>
2019-12-06 19:50:35 +0100

    commit05

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
             InitializeComponent();
             lblLastName.Text = Resource1.FullName; // label1
             btnAdd.Text = Resource1.Add; // button1
+            btnFW.Text = Resource1.FW; // button2
 
             // listbox1
             listUsers.DataSource = users;
@@ -34,6 +36,22 @@ namespace UserMaintenance
                 FullName = txtLastName.Text
             };
             users.Add(u);
+        }
+
+        private void btnFW_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sfd.FileName + ".txt");
+
+                foreach (User u in users)
+                {
+                    sw.WriteLine(u.ID + "; " + u.FullName);
+                }
+
+                sw.Close();
+            }
         }
     }
 }
--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -77,5 +77,14 @@ namespace UserMaintenance {
                 return ResourceManager.GetString("FullName", resourceCulture);
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Fájlba Írás.
+        /// </summary>
+        internal static string FW {
+            get {
+                return ResourceManager.GetString("FW", resourceCulture);
+            }
+        }
     }
 }
